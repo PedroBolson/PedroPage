5f62ee943e0d5922afef76af1c22c6deb9120ffc
PedroBolson <pedbolson@gmail.com>
2026-04-15 01:35:31 -0300

    Split on chunks

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,19 @@ export default defineConfig({
     tailwindcss(),
     react(),
   ],
+  build: {
+    rolldownOptions: {
+      output: {
+        advancedChunks: {
+          groups: [
+            { name: 'react-vendor', test: /node_modules\/(react|react-dom)\// },
+            { name: 'motion',       test: /node_modules\/motion\// },
+            { name: 'i18n',         test: /node_modules\/(react-i18next|i18next(-browser-languagedetector)?)\// },
+            { name: 'particles',    test: /node_modules\/@tsparticles\// },
+            { name: 'parallax',     test: /node_modules\/react-scroll-parallax\// },
+          ],
+        },
+      },
+    },
+  },
 })
